4c4bf2135796536bd53127b87db72beb15bf7970
Andrewyuraga <35347458+Andrewyuraga@users.noreply.github.com>
2018-09-09 16:15:13 +0300

    Update MassivePor.java

--- MassivePor.java
+++ MassivePor.java
@@ -4,18 +4,6 @@
  
 public class MassivePor {
     public static void main(String[] args) {
-//        int massive[] = new int [list.length];
-//        for (int i = 0; i < massive.length; i++) {
-//            System.out.println(massive[i] + " ");
-//        }
-//
-//        System.out.println(" ");
-//        for (int i = 5, j = massive.length - 1; i < massive.length; i++, j--) {
-//            massive[j] = massive[i];
-//            System.out.println(massive[j] + " ");
-//        }
-//    }
-//}
         int mas[] = new int[10];
         for (int i = 0; i < 10; i++) {
             mas[i] = (int) (Math.random() * 10 + 1);
